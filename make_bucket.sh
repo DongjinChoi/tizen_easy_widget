efc4a8a08cebd5d87e15838e36b791d3ce629078
DongjinChoi <milkelf.choi@gmail.com>
2016-05-20 12:12:34 +0900

    update rsync command & document

--- make_bucket.sh
+++ make_bucket.sh
@@ -6,9 +6,11 @@ gsutil mb gs://tizen-easy-widget-bucket
 #gsutil defacl set public-read gs://<your-bucket-name>
 gsutil defacl set public-read gs://tizen-easy-widget-bucket
 
-Upload items to the bucket. The rsync command is typically the fastest and easiest way to upload and update assets. You could also use cp.
+#Upload items to the bucket. The rsync command is typically the fastest and easiest way to upload and update assets. You could also use cp.
 #gsutil rsync -mr ./static gs://<your-bucket-name>/static
-gsutil rsync -mr ./static gs://tizen-easy-widget-bucket/static
+gsutil -m rsync -r -d ./static gs://tizen-easy-widget-bucket/static
+
+#You can now access your static assets via https://storage.googleapis.com/<your-bucket-name>/static/...
 
 
 
